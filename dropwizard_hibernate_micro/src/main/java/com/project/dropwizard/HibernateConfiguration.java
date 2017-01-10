89b7c29ee6ddbd8403d990895547102612bb7c97
ARMEN ARZUMANYAN <armen.arzumanyan.external@atos.net>
2017-01-10 10:43:40 +0400

    updated

--- dropwizard_hibernate_micro/src/main/java/com/project/dropwizard/HibernateConfiguration.java
+++ dropwizard_hibernate_micro/src/main/java/com/project/dropwizard/HibernateConfiguration.java
@@ -11,7 +11,7 @@ public class HibernateConfiguration extends Configuration {
     @Valid
     @NotNull
     @JsonProperty("database")
-    private DataSourceFactory database = new DataSourceFactory();
+    private final DataSourceFactory database = new DataSourceFactory();
 
     public DataSourceFactory getDataSourceFactory() {
         return database;
